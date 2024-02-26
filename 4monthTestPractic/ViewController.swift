c189cc9d2b3c6585bf2642535907359ba57ab767
Tima <158593312+OmurbekovTemirlan@users.noreply.github.com>
2024-02-26 15:35:47 +0600

    Initial Commit

---
+++ 4monthTestPractic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4monthTestPractic
+//
+//  Created by Apple on 26.2.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
